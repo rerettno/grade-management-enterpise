a78545203ee9423efa66347bf2fbbd1b98ebae87
Retno <hapsari.eno003@gmail.com>
2025-07-31 13:33:38 +0700

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from "@/components/layout/navbar";
 import DashboardPage from "./dashboard/page";
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <DashboardPage />
     </div>
   );
