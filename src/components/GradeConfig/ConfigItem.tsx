dc93adfba8aba9390c8c2ec013800f84736c136a
Retno <hapsari.eno003@gmail.com>
2025-07-31 23:18:42 +0700

    dnd

--- src/components/GradeConfig/ConfigItem.tsx
+++ src/components/GradeConfig/ConfigItem.tsx
@@ -4,7 +4,7 @@
 import type { GradeItem } from "@/stores/gradeConfigStore";
 import type { DragItem } from "./types";
 import { useRef } from "react";
-// import { useDrag, useDrop } from "react-dnd";
+import { useDrag, useDrop } from "react-dnd";
 import type { DropTargetMonitor, DragSourceMonitor } from "react-dnd";
 import { useGradeConfigStore } from "@/stores/gradeConfigStore";
 import { Box, Typography, Slider, Paper } from "@mui/material";
@@ -21,44 +21,44 @@ export default function ConfigItem({ item, index, moveCard, errors }: Props) {
     (state) => state.updatePercentage
   );
 
-  // const [, drop] = useDrop<DragItem, void, unknown>({
-  //   accept: "card",
-  //   hover(draggedItem: DragItem, monitor: DropTargetMonitor) {
-  //     if (!ref.current) return;
-  //     const dragIndex = draggedItem.index;
-  //     const hoverIndex = index;
-  //     if (dragIndex === hoverIndex) return;
+  const [, drop] = useDrop<DragItem, void, unknown>({
+    accept: "card",
+    hover(draggedItem: DragItem, monitor: DropTargetMonitor) {
+      if (!ref.current) return;
+      const dragIndex = draggedItem.index;
+      const hoverIndex = index;
+      if (dragIndex === hoverIndex) return;
 
-  //     const hoverRect = ref.current.getBoundingClientRect();
-  //     const hoverMiddleY = (hoverRect.bottom - hoverRect.top) / 2;
-  //     const clientOffset = monitor.getClientOffset();
-  //     if (!clientOffset) return;
-  //     const hoverClientY = clientOffset.y - hoverRect.top;
+      const hoverRect = ref.current.getBoundingClientRect();
+      const hoverMiddleY = (hoverRect.bottom - hoverRect.top) / 2;
+      const clientOffset = monitor.getClientOffset();
+      if (!clientOffset) return;
+      const hoverClientY = clientOffset.y - hoverRect.top;
 
-  //     if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return;
-  //     if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return;
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) return;
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) return;
 
-  //     moveCard(dragIndex, hoverIndex);
-  //     draggedItem.index = hoverIndex;
-  //   },
-  // });
+      moveCard(dragIndex, hoverIndex);
+      draggedItem.index = hoverIndex;
+    },
+  });
 
-  // const [{ isDragging }, drag] = useDrag({
-  //   type: "card",
-  //   item: { index, id: item.id },
-  //   collect: (monitor: DragSourceMonitor) => ({
-  //     isDragging: monitor.isDragging(),
-  //   }),
-  // });
+  const [{ isDragging }, drag] = useDrag({
+    type: "card",
+    item: { index, id: item.id },
+    collect: (monitor: DragSourceMonitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
 
-  // drop(drag(ref));
+  drop(drag(ref));
   return (
     <Paper
       ref={ref}
       elevation={2}
       sx={{
         p: 2,
-        //    opacity: isDragging ? 0.5 : 1,
+        opacity: isDragging ? 0.5 : 1,
         cursor: "move",
         display: "flex",
         flexDirection: "column",
